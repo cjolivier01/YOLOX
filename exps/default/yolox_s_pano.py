c42ce18c62b6de50f145b6406b3968167c2adcb8
Chris Olivier <cjolivier01@apache.org>
2024-11-30 10:32:33 -0800

    pano width

---
+++ exps/default/yolox_s_pano.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# Copyright (c) Megvii, Inc. and its affiliates.
+
+from yolox.exp import Exp as MyExp
+
+import os
+
+
+class Exp(MyExp):
+    def __init__(self):
+        super(Exp, self).__init__()
+        self.input_size = (1312, 480)
+        self.depth = 0.33
+        self.width = 0.50
+        self.exp_name = os.path.split(os.path.realpath(__file__))[1].split(".")[0]
